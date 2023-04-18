8f260899d865eb015860d3366feea76280813632
juanpialvarez <juanpabloalvarezv@gmail.com>
2023-04-18 15:18:39 -0600

    Fix bugg in verification

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App extends Component {
     const accessToken = localStorage.getItem("access_token");
     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
+    console.log(window.location.search);
     const code = searchParams.get("code");
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
     if ((code || isTokenValid) && this.mounted) {
